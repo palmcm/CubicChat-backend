0fa96763f6d84ad10301f3f4cb83b4c969dd0a5f
palmcm <palmermonkeyboy@gmail.com>
2023-04-09 17:03:14 +0700

    feat: add Types to getRecentGroups and change logic

--- src/route/group/getRecentGroups.ts
+++ src/route/group/getRecentGroups.ts
@@ -2,64 +2,72 @@ import { ChatRoomType } from '@prisma/client'
 import { Request, Response } from 'express'
 
 import prisma from '../../prisma'
+import { getRecentGroupDto } from '../../types/group.types'
 
 const getRecentGroups = async (req: Request, res: Response) => {
-    try{
-        const userId: string = res.locals.userId
-        const groups = await prisma.chatRoom.findMany({
-            where: {
-                User: {
-                    some: {
-                        userId: userId
-                    }
-                }
+  try {
+    const userId: string = res.locals.userId
+    const groups = await prisma.chatRoom.findMany({
+      where: {
+        User: {
+          some: {
+            userId: userId,
+          },
+        },
+      },
+      select: {
+        chatRoomId: true,
+        name: true,
+        chatRoomType: true,
+        messages: {
+          orderBy: { createdAt: 'desc' },
+          take: 1,
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+
+    groups.sort((a, b) => {
+      if (a.messages.length === 0 && b.messages.length === 0) return 0
+      if (a.messages.length === 0) return 1
+      if (b.messages.length === 0) return -1
+      return (
+        b.messages[0].createdAt.getTime() - a.messages[0].createdAt.getTime()
+      )
+    })
+
+    const recents: getRecentGroupDto[] = await Promise.all(
+      groups.map(async (group) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { messages, ...rest } = group
+        if (group.chatRoomType === ChatRoomType.GROUP)
+          return { ...rest, name: rest.name || 'Unknown' }
+        const otherUser = await prisma.chatRoom.findFirst({
+          where: {
+            chatRoomId: group.chatRoomId,
+          },
+          select: {
+            User: {
+              where: {
+                userId: {
+                  not: userId,
+                },
+              },
             },
-            select: {
-                chatRoomId: true,
-                name: true,
-                chatRoomType: true,
-            },
-            orderBy: {
-                updatedAt: 'desc'
-            },
+          },
         })
-        // loop for each group, if chatRoomType is private, change name to other user's name
-        for (let i = 0; i < groups.length; i++) {
-            if (groups[i].chatRoomType === ChatRoomType.PRIVATE) {
-                const otherUser = await prisma.user.findFirst({
-                    where: {
-                        AND: [
-                            {
-                                userId: {
-                                    not: userId
-                                }
-                            },
-                            {
-                                ChatRoom: {
-                                    some: {
-                                        chatRoomId: groups[i].chatRoomId
-                                    }
-                                }
-                            }
-                        ]
-                    },
-                    select: {
-                        username: true
-                    }
-                })
-                if(!otherUser) return res.status(500).send('Other user is not found')
-                groups[i].name = otherUser.username
-            }
-        }
+        if (otherUser === null) return { ...rest, name: 'Unknown' }
+        return { ...rest, name: otherUser.User[0].username }
+      }),
+    )
 
-
-
-        return res.status(200).send(groups)
-    }
-    catch(error){
-        console.log(error)
-        return res.status(500).send('Server error get recent groups endpoint')
-    }
+    return res.status(200).send({ groups: recents })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send('Server error get recent groups endpoint')
+  }
 }
 
 export default getRecentGroups
